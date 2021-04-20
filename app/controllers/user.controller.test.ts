fb83cd0de9fd761ed656a2a179944ffab3249c9e
Anton <antongavrilov88@gmail.com>
2021-04-20 21:31:37 +0200

    add rest-js (not implemented yet)

--- app/controllers/user.controller.test.ts
+++ app/controllers/user.controller.test.ts
@@ -1,6 +1,7 @@
-import {sum} from './dummy';
+import { app } from '../app'
+import {sum} from './dummy'
 
 test('Dummy unit test', () => {
-    const actual = sum(1, 2);
-    expect(actual).toBe(3);
+    const actual = sum(1, 2)
+    expect(actual).toBe(3)
 })
