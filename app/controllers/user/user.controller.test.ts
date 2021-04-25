3daae3ad67cd997329b8430a6e18e64a4cf41786
Anton <antongavrilov88@gmail.com>
2021-04-25 17:30:04 +0200

    add test app add test db config fix problems with async in jest

--- app/controllers/user/user.controller.test.ts
+++ app/controllers/user/user.controller.test.ts
@@ -1,6 +1,21 @@
-const sum = (a: number, b: number) => a + b
+import {db} from '../../models/indexTest'
+import {app} from '../../appTest'
+import {UserRoutes} from '../../routes/constans'
 
-test('Dummy unit test', () => {
-    const actual = sum(1, 2)
-    expect(actual).toBe(3)
+const request = require('supertest')
+
+describe('my api test', () => {
+    it('Post /users/', async () => {
+        await db.sequelize.sync({force: true})
+        const {status} = await request(app).post(UserRoutes.baseUserRoute + UserRoutes.root, {
+            headers: {
+                authorization: 'required'
+            },
+            body: {
+                displayName: 'required',
+                avatar: 'required'
+            }
+        })
+        expect(status).toEqual(400)
+    })
 })
