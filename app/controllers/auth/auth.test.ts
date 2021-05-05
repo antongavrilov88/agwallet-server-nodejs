c7efdf655ebad7504eefd16b80b3b42d7817c1c4
Anton <antongavrilov88@gmail.com>
2021-05-05 22:18:19 +0200

    add tests

--- app/controllers/auth/auth.test.ts
+++ app/controllers/auth/auth.test.ts
@@ -1,8 +1,8 @@
 import {AuthRoutes} from '../../routes/constans'
 import {app} from '../../app'
-import { db } from '../../models/index'
+import {db} from '../../models/index'
 
-const request = require("supertest")
+const request = require('supertest')
 
 const user = {
     email: 'Anton',
@@ -10,18 +10,17 @@ const user = {
     admin: true
 }
 
-beforeEach( async () => {
+beforeEach(async () => {
     await db.sequelize.sync({force: true}).then(() => {
     })
 })
 
 describe('jdfbh', () => {
-it("should return Hello Test", async (done) => {
-     
-    const data = await request(app)
-        .post(AuthRoutes.baseAuthRoute + AuthRoutes.signUp)
-        .send(user)
-        .set('Accept', 'application/json')
+    it('should return Hello Test', async (done) => {
+        const data = await request(app)
+            .post(AuthRoutes.baseAuthRoute + AuthRoutes.signUp)
+            .send(user)
+            .set('Accept', 'application/json')
         // .end(done)
         expect(data).toEqual(201)
         done()
--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,5 @@
 import {app} from './app'
-import { db } from './models/index'
+import {db} from './models/index'
 
 db.sequelize.sync({force: true}).then(() => {
 })
