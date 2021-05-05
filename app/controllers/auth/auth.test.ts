8cbdd0d0ede25a60f17aca1212ce3d67a3cec216
Anton <antongavrilov88@gmail.com>
2021-05-06 01:05:03 +0200

    add logout test

--- app/controllers/auth/auth.test.ts
+++ app/controllers/auth/auth.test.ts
@@ -9,7 +9,7 @@ const request = require('supertest')
 userRoutes(app)
 authRoutes(app)
 
-const user = {
+const signUpUser = {
     email: 'Anton',
     password: '1234',
     admin: true
@@ -30,7 +30,7 @@ describe('Auth API tests', () => {
     it('Should return 201 response status and Anton as email to signup request with valid data', async (done) => {
         await request(app)
             .post(AuthRoutes.baseAuthRoute + AuthRoutes.signUp)
-            .send(user)
+            .send(signUpUser)
             .set('Accept', 'application/json')
             .then((data: any) => {
                 expect(data.status).toEqual(201)
@@ -41,20 +41,38 @@ describe('Auth API tests', () => {
     it('Should return 409 response status to signup request with existing email', async (done) => {
         await request(app)
             .post(AuthRoutes.baseAuthRoute + AuthRoutes.signUp)
-            .send(user)
+            .send(signUpUser)
             .set('Accept', 'application/json')
             .then((data: any) => {
                 expect(data.status).toEqual(409)
             })
         done()
     })
-    it('Should return 409 response status to signin request with user alredy in system', async (done) => {
+    it('Should return 409 response status to signin request with user already in system', async (done) => {
         await request(app)
             .post(AuthRoutes.baseAuthRoute + AuthRoutes.signIn)
             .send(signInUser)
             .set('Accept', 'application/json')
             .then((data: any) => {
                 expect(data.status).toEqual(409)
+            })
+        done()
+    })
+    it('Should return 200 response status to signout request with user already in system', async (done) => {
+        await db.sequelize.sync({force: true}).then(() => {
+        })
+        const token = await request(app)
+            .post(AuthRoutes.baseAuthRoute + AuthRoutes.signUp)
+            .send(signUpUser)
+            .set('Accept', 'application/json')
+            .then((data: any) => JSON.parse(data.text).data.token)
+        await request(app)
+            .post(AuthRoutes.baseAuthRoute + AuthRoutes.signOut)
+            .send()
+            .set('Authorization', `Bearer ${token}`)
+            .set('Accept', 'application/json')
+            .then((data: any) => {
+                expect(data.status).toEqual(200)
             })
         done()
     })
