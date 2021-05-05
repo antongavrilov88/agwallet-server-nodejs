bec1a42d1a704aedd729f2698915f399dbfc1eb5
Anton <antongavrilov88@gmail.com>
2021-05-06 00:22:39 +0200

    add conflict signup test case

--- app/controllers/auth/auth.test.ts
+++ app/controllers/auth/auth.test.ts
@@ -15,20 +15,30 @@ const user = {
     admin: true
 }
 
-beforeEach(async (done) => {
+beforeAll(async (done) => {
     await db.sequelize.sync({force: true}).then(() => {
     })
     done()
 })
 
-describe('jdfbh', () => {
-    it('should return Hello Test', async (done) => {
+describe('Auth API tests', () => {
+    it('Should return 201 response status to signup request with valid data', async (done) => {
         await request(app)
             .post(AuthRoutes.baseAuthRoute + AuthRoutes.signUp)
             .send(user)
             .set('Accept', 'application/json')
             .then((data: any) => {
                 expect(data.status).toEqual(201)
+            })
+        done()
+    })
+    it('Should return 409 response status to signup request with existing email', async (done) => {
+        await request(app)
+            .post(AuthRoutes.baseAuthRoute + AuthRoutes.signUp)
+            .send(user)
+            .set('Accept', 'application/json')
+            .then((data: any) => {
+                expect(data.status).toEqual(409)
             })
         done()
     })
