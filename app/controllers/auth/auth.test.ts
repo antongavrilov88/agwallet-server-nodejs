070212ff1268316d26623c80a5f5ed3181090d27
Anton <antongavrilov88@gmail.com>
2021-05-06 00:27:43 +0200

    add signup response body test case

--- app/controllers/auth/auth.test.ts
+++ app/controllers/auth/auth.test.ts
@@ -22,13 +22,14 @@ beforeAll(async (done) => {
 })
 
 describe('Auth API tests', () => {
-    it('Should return 201 response status to signup request with valid data', async (done) => {
+    it('Should return 201 response status and Anton as email to signup request with valid data', async (done) => {
         await request(app)
             .post(AuthRoutes.baseAuthRoute + AuthRoutes.signUp)
             .send(user)
             .set('Accept', 'application/json')
             .then((data: any) => {
                 expect(data.status).toEqual(201)
+                expect(JSON.parse(data.text).data.user.email).toEqual('Anton')
             })
         done()
     })
