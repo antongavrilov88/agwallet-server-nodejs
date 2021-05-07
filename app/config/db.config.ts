584f7cd0a13673e89c296c8d9b8ed63d7cf17d86
Anton <antongavrilov88@gmail.com>
2021-05-07 14:47:45 +0200

    turn db connection options to variables

--- app/config/db.config.ts
+++ app/config/db.config.ts
@@ -2,13 +2,16 @@ import {Sequelize, SequelizeOptions} from 'sequelize-typescript'
 
 const HOST = '127.0.0.1'
 const PORT = process.env.NODE_ENV === 'development' ? 5437 : 5438
+const USERNAME = 'sampleUser'
+const PASSWORD = 'password'
+const DATABASE = 'sampledb'
 
 const sequelizeOptions: SequelizeOptions = {
     host: HOST,
     port: PORT,
-    username: 'sampleUser',
-    password: 'password',
-    database: 'sampledb',
+    username: USERNAME,
+    password: PASSWORD,
+    database: DATABASE,
 
     dialect: 'postgres',
     pool: {
