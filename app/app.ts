0b09225c0e2847b6472be1851887ec8fbe0a4188
Anton <antongavrilov88@gmail.com>
2021-05-10 23:13:38 +0200

    remove useless cors policy

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors');
 
 export const app = express()
 
-const corsOptions = {
-    origin: 'https://localhost:5000'
-}
-
-app.use(cors(corsOptions));
-
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended: true}))
 
