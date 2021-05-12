564cb1f8ec0f43348a7e7176e755090dc332972e
Anton <antongavrilov88@gmail.com>
2021-05-13 00:13:56 +0200

    move all models to one file

--- app/models/index.ts
+++ app/models/index.ts
@@ -1,8 +1,40 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import {sequelize} from '../config/db.config'
-import {tokenModel} from './token.model'
-import {userModel} from './user.model'
 
 const Sequelize = require('sequelize')
+
+export const tokenModel = (sequelize: any, Sequelize: any) => {
+    const Token = sequelize.define('tokens', {
+        token: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        userId: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        }
+    })
+    return Token
+}
+
+export const userModel = (sequelize: any, Sequelize: any) => {
+    const User = sequelize.define('users', {
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        admin: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        }
+    })
+
+    return User
+}
 
 export const db:any = {}
 
