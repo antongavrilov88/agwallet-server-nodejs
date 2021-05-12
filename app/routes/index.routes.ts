cfac11471dd00d9454883e8aa31dcb713a454d7e
Anton <antongavrilov88@gmail.com>
2021-05-12 22:55:44 +0200

    add index router

---
+++ app/routes/index.routes.ts
@@ -0,0 +1,12 @@
+import {authRoutes} from './auth.routes'
+import {userRoutes} from './user.routes'
+
+const routes = [
+    authRoutes,
+    userRoutes
+]
+
+export const router = (app: any) => {
+    routes.map(route => route(app))
+    return app
+}
